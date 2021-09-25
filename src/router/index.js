049b1f80b090900b9b35d97b95448ece0a7c5805
Jacob Parra <jacobisraelparra@gmail.com>
2021-09-24 19:36:58 -0700

    vue router fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Services from "../views/Services.vue";
 
 Vue.use(VueRouter);
 
@@ -22,11 +23,7 @@ const routes = [
   {
     path: "/services",
     name: "Services",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Services.vue"),
+    component: Services,
   },
   {
     path: "/portal",
@@ -59,7 +56,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.publicPath,
+  base: process.env.BASE_URL,
   routes
 });
 
