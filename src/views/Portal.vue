c98c8a7ed53819b317dfe13e2952d8cc5d37383f
Omar Reyes <reyomar80@hotmail.com>
2021-10-05 11:18:45 -0700

    Created client portal page

--- src/views/Portal.vue
+++ src/views/Portal.vue
@@ -1,5 +1,73 @@
 <template>
-    <div class="portal">
-      <h1>Portal page</h1>
-    </div>
+  <div class="background">
+    <b-container>
+      <div class="pt-5 pb-5">
+        <b-form
+          class="login-form mx-auto my-auto pr-5 pl-5"
+          style="width: 70vw; height: 80vh;"
+          @submit="onSubmit"
+          @reset="onReset"
+          target="_blank"
+          action="https://secure.petexec.net/remoteLogin.php"
+          method="post"
+          v-if="show"
+        >
+          <h3 class="mt-5 text-primary">Login</h3>
+          <img src="@/assets/petexeclogo.png" />
+          <div class="mt-5 px-xl-5 mx-xl-5">
+            <b-form-group id="input-group-1">
+              <b-form-input
+                id="input-1"
+                v-model="form.username"
+                type="text"
+                placeholder="Username"
+                required
+                name="username"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2">
+              <b-form-input
+                id="input-2"
+                v-model="form.password"
+                required
+                name="password"
+                type="password"
+                placeholder="Password"
+              ></b-form-input>
+            </b-form-group>
+          </div>
+          <b-button type="submit" variant="primary">Login</b-button>
+        </b-form>
+      </div>
+    </b-container>
+  </div>
 </template>
+
+<style lang="scss">
+body {
+  background-image: url("~@/assets/portal-background.jpeg");
+  background-size: cover;
+}
+.login-form {
+  background: rgba(255, 255, 255, 0.5);
+  border: 2px solid #fff;
+  border-radius: 25px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+
+      show: true,
+    };
+  },
+  methods: {},
+};
+</script>
